a81956fceeca5bc1b023ce1a9e34e31856586bbf
remidbs <remi.delbouys@laposte.net>
2021-05-06 13:47:41 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     url="https://github.com/envinorma/text_diff",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
